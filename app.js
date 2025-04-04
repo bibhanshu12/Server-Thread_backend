2afb12625282f227cf4774daec173152b48f649f
bibhanshu12 <mail.bibhanshu127@gmail.com>
2025-04-05 03:45:55 +0530

    securities improved!

--- app.js
+++ app.js
@@ -3,23 +3,32 @@ const dotenv=require("dotenv")
 const connectDB=require('./config/db');
 const router = require('./routes');
 const cors=require('cors')
+const rateLimit = require('express-rate-limit');
 const cookieParser = require("cookie-parser");
+const helmet = require('helmet');
 
 
 dotenv.config();
 const app =express();
 const port=3000;
- 
+
+app.use(helmet());
 app.use(cors({
   origin: 'https://threads.bibhanshu.tech', // Replace with your frontend URL
   credentials:true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(cookieParser());
- 
+
 connectDB();
 app.use(express.json());
+
+app.use(cookieParser());
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+app.use('/api', apiLimiter);
 
 app.use('/api',router) 
 
